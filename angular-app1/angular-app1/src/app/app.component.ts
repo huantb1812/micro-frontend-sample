237e670a85b5f65ea50e62076e3bdf60810d50a1
Nguyen Van Huan <huantb1812@gmail.com>
2023-06-14 22:53:53 +0800

    .

--- angular-app1/angular-app1/src/app/app.component.ts
+++ angular-app1/angular-app1/src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-app1';
+  data = 'data app 1';
+  keyname='app1';
+  onsave(){
+    localStorage.setItem(this.keyname, this.data);
+  }
+  onget(){
+   this.data = localStorage.getItem(this.keyname)||'not found';
+  }
 }
--- angular-app2/angular-app2/src/app/app.component.ts
+++ angular-app2/angular-app2/src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-app2';
+  data = 'data app 2';
+  keyname = 'app2';
+  onsave() {
+    localStorage.setItem(this.keyname, this.data);
+  }
+  onget() {
+    this.data = localStorage.getItem(this.keyname) || 'not found';
+  }
 }
